4ec7d2e0e0539c09b74beb0931b34e4cb91269a3
Jonathan Clark <hello@doitalldev.com>
2024-11-26 16:02:55 -0700

    feat: tmux and stuff

---
+++ nvim/lua/keymaps/navigator.lua
@@ -0,0 +1,5 @@
+vim.keymap.set('n', "<c-h>", "<cmd>TmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set('n', "<c-j>", "<cmd>TmuxNavigateDown<CR>", { silent = true })
+vim.keymap.set('n', "<c-k>", "<cmd>TmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set('n', "<c-l>", "<cmd>TmuxNavigateRight<CR>", { silent = true })
+
